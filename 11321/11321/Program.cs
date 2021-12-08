6b9c13530f2bad3b4cd7eb9c039b6699ada47d2e
Nastya <A.B@gmail.com>
2021-12-08 17:55:46 +0200

    init

---
+++ 11321/11321/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _11321
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logarithm calculation");
+            Console.Write("Enter x:");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Enter base of logarithm:");
+            double baze = double.Parse(Console.ReadLine());
+
+            double result = Math.Log(x, baze);
+            Console.WriteLine(result);
+
+        }
+    }
+}
